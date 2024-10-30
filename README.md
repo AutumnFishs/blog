59ad77d0cb1fa4d7c77e2837b5dd72055e23c0a3
何英杰 <305664823@qq.com>
2024-10-30 21:39:50 +0800

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# 博客笔记
+当前项目为个人博客笔记，用于记录学习过程，方便日后查阅。
+
+## 介绍
+- 基于 `vitepress` 搭建的个人博客
+- 部署在 `GitHub Pages` 、`vercel` 上，以及通过 `docker + ngrox` 部署到自己电脑上
+- 主题使用 `@sugarat/theme` 
+- 博客笔记采用Gitalk进行评论功能，使用Valine进行访问量统计功能。
+
+## 目录
+- 
--- docs/.vitepress/theme/config/nav.ts
+++ docs/.vitepress/theme/config/nav.ts
@@ -5,14 +5,8 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: "导航", link: "/navpage/index.html" },
   {
     text: "前端",
-    items: [
-      { text: "Vue笔记", link: "/frontend/vue/vue.html" },
-      { text: "React笔记", link: "/frontend/react/react.html" },
-      { text: "Nginx笔记", link: "/frontend/nginx/nginx.html" },
-      { text: "VitePress笔记", link: "/frontend/vitepress/vitepress.html" },
-    ],
+    link: "/frontend/index.html",
   },
-  { text: "面试题", link: "/interview/index.html" },
-  { text: "随手日志", link: "/fragments/index.html" },
-  { text: "关于我的", link: "https://github.com/AutumnFishs" },
+  { text: "日志", link: "/fragments/index.html" },
+  { text: "其他", link: "/fragments/index.html" },
 ];
